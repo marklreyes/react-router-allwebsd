acc404d7ab78e7b4943d3988753e903b62fe78b5
Mark Reyes <mr@marklreyes.com>
2025-02-10 11:35:50 -0800

    Modify contents of Welcome component with Toast Message

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,46 +1,61 @@
+import { useState } from "react";
 import { CiHeadphones } from "react-icons/ci";
 import { CiMail } from "react-icons/ci";
 
-import logoAllWebSD from "../../public/logo-allwebsd.png";
-
 const linkStyles = "group flex items-center gap-3 self-stretch p-3 leading-normal text-[#FFC426] hover:underline";
 
+const resources = [
+  {
+    href: "https://www.marklreyes.com/allwebsd-podcast/",
+    text: "Listen on MarkLReyes.com",
+    icon: (
+			<CiHeadphones />
+    ),
+  },
+  {
+    href: "mailto:mr@marklreyes.com",
+    text: "Contact Me",
+    icon: (
+			<CiMail />
+    ),
+  },
+];
+
 export function Welcome() {
+	const [showToast, setShowToast] = useState(true);
+
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main className="flex items-center justify-center pt-4 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoAllWebSD}
-              alt="AllWebSD Just a web developer storytelling out of America's Finest City!"
-              className="block w-full"
-            />
-          </div>
-        </header>
         <div className="max-w-[600px] w-full space-y-6 px-4">
-					<nav className="rounded-3xl border border-white p-6 dark:border-white space-y-4">
-            <p className="leading-6 text-white text-center">
-              Thanks for stopping by! But what now?
-            </p>
-						<p className="text-white">
-							The "OG" site was a single <code>index.html</code> file styled with Bootstrap 4 with navigation tabs that would turn into an accordion for mobile. It was also a PWA that you could bookmark to your phone's home screen. 📲
-						</p>
-						<p className="text-white">
-							This new site is aimed at leveraging a React Router setup housed within GitHub and then deployed to Netlify. 🚀
-						</p>
-						<p className="text-white">
-							But...what does that mean?! 😂
-						</p>
-						<p className="text-white">
-							The site is <em>Under Construction</em>. 🚧
-						</p>
-						<p className="text-white">
-							Click the links below to listen or get ahold of me. Thanks and Aloha! 🤙
-						</p>
-						<div>
-							<iframe src="//castbox.fm/app/castbox/player/id2933770?v=8.22.10&autoplay=0" width="100%" height="500"></iframe>
+				{showToast && (
+					<div className="toast toast-top toast-center w-full max-w-xs md:max-w-md">
+						<div className="alert alert-info p-2 md:p-4">
+							<div className="flex flex-col sm:flex-row items-start sm:items-center justify-between w-full gap-2">
+								<span className="text-xs sm:text-sm md:text-base whitespace-normal break-words">
+								🎙️ Refactoring to React Router with Tailwind &amp; DaisyUI!
+								&nbsp;
+									<a
+										href="https://github.com/marklreyes/react-router-allwebsd/commits/main/"
+										target="_blank"
+										rel="noreferrer"
+										className="underline hover:text-white"
+									>
+										View Commits
+									</a>
+								</span>
+								<button
+									className="btn btn-circle btn-xs md:btn-sm self-end sm:self-auto"
+									onClick={() => setShowToast(false)}
+								>
+									✕
+								</button>
+							</div>
 						</div>
+					</div>
+				)}
+					<nav className="rounded-3xl border border-white p-6 dark:border-white space-y-4">
+						<iframe title="AllWebSD Castbox Player" src="//castbox.fm/app/castbox/player/id2933770?v=8.22.10&autoplay=0" width="100%" height="500"></iframe>
             <ul>
               {resources.map(({ href, text, icon }) => (
                 <li key={href}>
@@ -62,20 +77,3 @@ export function Welcome() {
     </main>
   );
 }
-
-const resources = [
-  {
-    href: "https://www.marklreyes.com/allwebsd-podcast/",
-    text: "Listen on MarkLReyes.com",
-    icon: (
-			<CiHeadphones />
-    ),
-  },
-  {
-    href: "mailto:mr@marklreyes.com",
-    text: "Contact Me",
-    icon: (
-			<CiMail />
-    ),
-  },
-];
