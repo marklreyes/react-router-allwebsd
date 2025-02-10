f515aa75ec5c05208e6e8fef25f90f9af4795639
Mark Reyes <mr@marklreyes.com>
2025-02-10 15:44:20 -0800

    Add more meta tags

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,6 +5,11 @@ export function meta({}: Route.MetaArgs) {
   return [
     { title: "Web Developer podcasting out of San Diego, California | AllWebSD.com" },
     { name: "description", content: "Just a web developer podcasting out of America's Finest City!" },
+		{ name: "twitter:card", content: "Just a web developer storytelling out of America's Finest City!" },
+		{ property: "og:title", content: "Just a web developer storytelling out of America's Finest City!" },
+		{ property: "og:type", content: "website" },
+		{ property: "og:url", content: "//www.allwebsd.com" },
+		{ property: "og:description", content: "Just a web developer storytelling out of America's Finest City!" },
   ];
 }
 
