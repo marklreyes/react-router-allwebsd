f49f5365ee830e1ea535168f6d81343366f5e193
Mark Reyes <mr@marklreyes.com>
2025-02-10 12:13:28 -0800

    Set up routes for Guests, Sponsors, Contact

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+	index("routes/home.tsx"),
+	route("guests", "guests/Guests.tsx"),
+	route("sponsors", "sponsors/Sponsors.tsx"),
+	route("contact", "contact/Contact.tsx"),
+
+] satisfies RouteConfig;
