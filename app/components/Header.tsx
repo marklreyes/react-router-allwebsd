53c89384926d6787a0c1ddfa64280e258082f447
Mark Reyes <mr@marklreyes.com>
2025-02-10 11:34:59 -0800

    Modify Header to include nav links and logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logoAllWebSD from "../../public/logo-allwebsd.png";
 
 export default function Header() {
   return (
     <header className="bg-white text-base-100 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        {/* <h1 className="text-2xl font-heading">AllWebSD</h1> */}
-        {/* <nav>
+			<div className="w-32 bg-[#3D2F26] p-2 rounded">
+				<NavLink to="/">
+					<img
+						src={logoAllWebSD}
+						alt="AllWebSD Logo"
+						className="w-full h-auto"
+					/>
+				</NavLink>
+			</div>
+
+        <nav>
           <NavLink to="/" className={({ isActive }) => isActive ? "mr-4 font-bold" : "mr-4"}>Home</NavLink>
-          <NavLink to="/about" className={({ isActive }) => isActive ? "mr-4 font-bold" : "mr-4"}>About</NavLink>
+          <NavLink to="/guests" className={({ isActive }) => isActive ? "mr-4 font-bold" : "mr-4"}>Guests</NavLink>
+          <NavLink to="/sponsors" className={({ isActive }) => isActive ? "mr-4 font-bold" : "mr-4"}>Sponsors</NavLink>
           <NavLink to="/contact" className={({ isActive }) => isActive ? "mr-4 font-bold" : "mr-4"}>Contact</NavLink>
-        </nav> */}
+        </nav>
       </div>
     </header>
   );
