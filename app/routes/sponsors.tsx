086447c83a2e1c289756b275bacf7950b152ae5b
Mark Reyes <mr@marklreyes.com>
2025-02-11 11:18:41 -0800

    Stub in HTML placeholders for consistency on other pages

--- app/routes/sponsors.tsx
+++ app/routes/sponsors.tsx
@@ -14,8 +14,12 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Sponsors() {
 	return (
-		<>
-		Sponsors Page
-		</>
+		<div className="flex items-center justify-center pt-4 pb-4">
+			<div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+				<div className="max-w-[1024px] w-full px-4">
+					<p>The future home of a sponsors page.</p>
+				</div>
+			</div>
+		</div>
 	);
 }
