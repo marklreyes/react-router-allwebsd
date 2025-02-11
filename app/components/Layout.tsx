e480814127ef923d03b82fa09b8f08d0b67dada9
Mark Reyes <mr@marklreyes.com>
2025-02-10 17:50:55 -0800

    Remove unused React import

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Loading from "./Loading";
