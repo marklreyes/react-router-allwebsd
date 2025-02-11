cba5ebf908255afd7bdb84bf3cf7682240750532
Mark Reyes <mr@marklreyes.com>
2025-02-11 11:00:32 -0800

    Add banner image to aside container

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -30,15 +30,35 @@ export function Layout() {
         <Links />
       </head>
       <body>
-        {isLoading ? <Loading /> : (
-          <>
-            <Header />
-            <main className="container mx-auto p-4">
-			  			<Outlet />
-            </main>
-            <Footer />
-          </>
-        )}
+				{isLoading ? <Loading /> : (
+					<>
+						<Header />
+						<div className="container mx-auto p-4">
+							<div className="grid grid-cols-1 lg:grid-cols-12 gap-4">
+								<main className="col-span-1 lg:col-span-9">
+									<Outlet />
+								</main>
+								<aside className="col-span-1 lg:col-span-3 bg-[#3D2F26] rounded-lg p-4">
+									<div className="w-[330px] h-[250px] mx-auto mb-4">
+										<a
+											href="https://sdfutures.org/support-sdff"
+											target="_blank"
+											rel="noopener noreferrer"
+											className="block hover:opacity-80 transition-opacity"
+										>
+											<img
+												src="/images/SDFF_banner.jpg"
+												alt="San Diego Futures Foundation join the movement and bridge the digital divide"
+												className="w-full h-full object-cover rounded"
+											/>
+										</a>
+									</div>
+								</aside>
+							</div>
+						</div>
+						<Footer />
+					</>
+				)}
         <ScrollRestoration />
         <Scripts />
       </body>
