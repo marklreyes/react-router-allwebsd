7338ec10ea8ac155629cd1a007f6937be49e5180
Mark Reyes <mr@marklreyes.com>
2025-02-16 11:11:01 -0800

    Remove padding on Pages containers

--- app/pages/about-me/about-me.tsx
+++ app/pages/about-me/about-me.tsx
@@ -7,7 +7,7 @@ export function AboutMe() {
 	return (
 		<div className="flex items-center justify-center">
 			<div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-				<div className="w-full px-4">
+				<div className="w-full">
 					<h1 className="text-3xl md:text-4xl font-bold text-white mb-6 text-center">
 						👋🏾, I'm Mark!
 					</h1>
