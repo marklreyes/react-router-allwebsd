b2facb9a9ecb6d3ff72088d0dc69e44f9e2f8723
Mark Reyes <mr@marklreyes.com>
2025-02-18 14:03:43 -0800

    Add HeroBanner as a Component for Welcome Page

---
+++ app/components/HeroBanner.tsx
@@ -0,0 +1,11 @@
+export function HeroBanner() {
+	return(
+		<div className="w-full mb-4">
+			<img
+				src="/images/allwebsd_hero_banner.jpg"
+				alt="Just a web developer storytelling out of America's Finest City."
+				className="rounded-lg shadow-lg"
+			/>
+	  </div>
+	);
+}
--- app/pages/welcome/welcome.tsx
+++ app/pages/welcome/welcome.tsx
@@ -3,42 +3,47 @@ import { NavLink } from "react-router-dom";
 import { CiHeadphones } from "react-icons/ci";
 import { CiMail } from "react-icons/ci";
 import { Toast } from "../../components/Toast";
+import { HeroBanner } from "../../components/HeroBanner";
 
 export function Welcome() {
 	const [showToast, setShowToast] = useState(true);
 
   return (
+	<>
+		<HeroBanner />
 		<div className="flex items-center justify-center">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <div className="w-full">
-				<Toast showToast={showToast} setShowToast={setShowToast} />
+			<div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+				<div className="w-full">
+					<Toast showToast={showToast} setShowToast={setShowToast} />
 					<nav className="rounded-lg border border-white p-6 dark:border-white space-y-4">
 						<iframe title="AllWebSD Castbox Player" src="//castbox.fm/app/castbox/player/id2933770?v=8.22.10&autoplay=0" width="100%" height="500"></iframe>
-            <ul>
-							<li>
-								<a
-									className="group flex items-center gap-3 self-stretch p-3 leading-normal text-[#FFC425] hover:text-white underline hover:no-underline"
-									href="https://www.marklreyes.com/allwebsd-podcast/"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									<CiHeadphones />
-									Listen on MarkLReyes.com
-                </a>
-							</li>
-							<li>
-								<NavLink
-									to="/contact"
-									className="group flex items-center gap-3 self-stretch p-3 leading-normal text-[#FFC425] hover:text-white underline hover:no-underline"
-								>
-									<CiMail />
-									Contact Me
-								</NavLink>
-							</li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
+						<ul>
+										<li>
+											<a
+												className="group flex items-center gap-3 self-stretch p-3 leading-normal text-[#FFC425] hover:text-white underline hover:no-underline"
+												href="https://www.marklreyes.com/allwebsd-podcast/"
+												target="_blank"
+												rel="noopener noreferrer"
+											>
+												<CiHeadphones />
+												Listen on MarkLReyes.com
+							</a>
+										</li>
+										<li>
+											<NavLink
+												to="/contact"
+												className="group flex items-center gap-3 self-stretch p-3 leading-normal text-[#FFC425] hover:text-white underline hover:no-underline"
+											>
+												<CiMail />
+												Contact Me
+											</NavLink>
+										</li>
+						</ul>
+				</nav>
+				</div>
+			</div>
+		</div>
+	</>
+
   );
 }
