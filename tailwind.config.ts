7a8ba53ba3ff822eedceaf5688c14f27b0147789
Mark Reyes <mr@marklreyes.com>
2025-02-19 17:26:29 -0800

    Add Tailwind Plugin which dangerouslySetInnerHTML contents can use for CSS utility classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -38,6 +38,7 @@ export default {
   },
   plugins: [
 		require("daisyui"),
+		require("@tailwindcss/typography"),
 	],
 	daisyui: {
     themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
