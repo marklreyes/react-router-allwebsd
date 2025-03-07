f0038cd142cd24d5ac05d48e03dcc06abfae0004
Mark Reyes <mark.l.reyes@gmail.com>
2025-03-07 09:42:34 -0800

    Update configs to align with Tailwind CSS v3 to v4, daisyUI v4 to v5

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
@@ -8,10 +8,10 @@ import netlifyPlugin from "@netlify/vite-plugin-react-router";
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin()],
+  plugins: [reactRouter(), tsconfigPaths(), netlifyPlugin(), tailwindcss()],
   build: {
     rollupOptions: {
       output: {
